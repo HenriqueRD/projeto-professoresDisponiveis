0f0856e998135c4e94bd85b5c0723d18aaa4410b
HenriqueRD <henriquethedordox@gmail.com>
2023-10-30 19:30:53 -0300

    add cors

--- server-node/src/server.ts
+++ server-node/src/server.ts
@@ -1,8 +1,11 @@
 import express from 'express'
+import cors from 'cors'
 import routes from './routes'
 
 const api = express()
 
-api.listen(3333)
+api.use(cors())
 api.use(express.json())
 api.use(routes)
+
+api.listen(3333)
